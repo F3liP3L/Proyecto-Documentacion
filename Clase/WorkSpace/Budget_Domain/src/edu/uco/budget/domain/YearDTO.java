758440bd67a055f292e4b00139446b9c5ec9eb42
Danny23A <100450486+Danny23A@users.noreply.github.com>
2022-10-14 16:04:38 -0500

    Diagrama Entidad Relacion
    Modificacion diagrama entidad relacion

---
+++ Clase/WorkSpace/Budget_Domain/src/edu/uco/budget/domain/YearDTO.java
@@ -0,0 +1,46 @@
+package edu.uco.budget.domain;
+
+import java.util.UUID;
+
+import static edu.uco.budget.crosscutting.helper.NumberHelper.isLessOrEqualsThan;
+import static edu.uco.budget.crosscutting.helper.NumberHelper.ZERO;
+
+import static edu.uco.budget.crosscutting.helper.UUIDHelper.getDefaultUUID;
+import static edu.uco.budget.crosscutting.helper.UUIDHelper.getNewUUID;
+
+public final class YearDTO {
+		
+	private UUID id;
+	private short yearNumber;
+	
+	public YearDTO(){
+		setId(getNewUUID());
+		setYearNumber(ZERO);
+	}
+	
+	public static final YearDTO create (final UUID id, final short yearNumber) {
+		return new YearDTO(id, yearNumber);
+	}
+	
+	public YearDTO(final UUID id, final short yearNumber) {
+		setId(id);
+		setYearNumber(yearNumber);
+	}
+
+	public final UUID getId() {
+		return id;
+	}
+
+	public final void setId(UUID id) {
+		this.id = getDefaultUUID(id);
+	}
+
+	public final short getYearNumber() {
+		return yearNumber;
+	}
+
+	public final void setYearNumber(short yearNumber) {
+		this.yearNumber = isLessOrEqualsThan(yearNumber,ZERO)? ZERO: yearNumber;
+	}
+	
+}
---
+++ Clase/WorkSpace/Budget_Domain/src/edu/uco/budget/domain/builder/YearDTOBuilder.java
@@ -0,0 +1,35 @@
+package edu.uco.budget.domain.builder;
+
+import java.util.UUID;
+
+import edu.uco.budget.domain.YearDTO;
+
+public class YearDTOBuilder {
+	
+	private UUID id;
+	private short yearNumber;
+	
+	private YearDTOBuilder() {
+		super();
+	}
+	
+	public static final YearDTOBuilder getYearDTOBuilder() {
+		return new YearDTOBuilder();
+	}
+	
+	public final YearDTOBuilder setId(final UUID id) {
+		this.id = id;
+		return this;
+	}
+
+	public final YearDTOBuilder setYearNumber(final short yearNumber) {
+		this.yearNumber = yearNumber;
+		return this;
+	}
+
+	public final YearDTO build() {
+		return YearDTO.create(id, yearNumber);
+	}
+	
+	
+}
