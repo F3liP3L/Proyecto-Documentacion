868340712e622f67b54eabb27dc8d7d605ed003c
Felipe Cardona <felipe7u7432@gmail.com>
2022-12-04 03:35:39 -0500

    Mas Clean Code
    Se borran metodos main y comentarios

--- Extra Clase/Proyect_QuickJob/Quickjob_Service/src/edu/uco/quickjob/service/bussines/identificationdocument/implementation/FindIdentificationDocumentByNumberAndTypeUseCaseImpl.java
+++ Extra Clase/Proyect_QuickJob/Quickjob_Service/src/edu/uco/quickjob/service/bussines/identificationdocument/implementation/FindIdentificationDocumentByNumberAndTypeUseCaseImpl.java
@@ -1,13 +1,9 @@
 package edu.uco.quickjob.service.bussines.identificationdocument.implementation;
 
 import java.util.List;
-import java.util.UUID;
 
-import edu.uco.quickjob.crosscutting.helper.UUIDHelper;
 import edu.uco.quickjob.data.dao.factory.DAOFactory;
-import edu.uco.quickjob.data.enumeration.DAOFactoryType;
 import edu.uco.quickjob.domain.IdentificationDocumentDTO;
-import edu.uco.quickjob.domain.IdentificationTypeDTO;
 import edu.uco.quickjob.service.bussines.identificationdocument.FindIdentificationDocumentByNumberAndType;
 import edu.uco.quickjob.service.bussines.identificationdocument.FindIdentificationDocumentUseCase;
 
@@ -34,20 +30,5 @@ public class FindIdentificationDocumentByNumberAndTypeUseCaseImpl implements Fin
 	
 	private static boolean findIdentificationTypeExist (List<IdentificationDocumentDTO> result, IdentificationDocumentDTO document) {
 		return !result.stream().filter(doc -> doc.getIdentificationNumber().equalsIgnoreCase(document.getIdentificationNumber())).toList().isEmpty();
-	}
-	
-	public static void main(String[] args) {
-		DAOFactory factory = DAOFactory.getDAOFactory(DAOFactoryType.POSTGRESQL);
-		IdentificationDocumentDTO document = new IdentificationDocumentDTO();
-		factory.openConnection();
-		// factory.getIdentificationTypeDAO().find().forEach(elem -> System.out.println(elem.getIdAsString() + " -- " + elem.getName()));;
-		UUID id = UUIDHelper.getUUIDFromString("df5659d3-ab5b-45f2-a9f4-4329180cddd5");
-		IdentificationTypeDTO documentTypeDTO = IdentificationTypeDTO.create(id);
-		document.setIdentificationType(documentTypeDTO);
-		document.setIdentificationNumber("1000101338");
-		List<IdentificationDocumentDTO> results = factory.getIdentificationDocumentDAO().find(document); //.forEach(e -> System.out.println(e.getIdAsString() + " -- " + e.getSurname() + " -- digitos: " + e.getIdentificationNumber() + " -- Type: "+ e.getIdentificationType().getName()));
-		results.forEach(e -> System.out.println(e.getIdAsString() + " -- " + e.getSurname() + " -- digitos: " + e.getIdentificationNumber() + " -- Type: "+ e.getIdentificationType().getName()));
-		System.out.println(findIdentificationTypeExist(results, document));
-		
 	}
 }
